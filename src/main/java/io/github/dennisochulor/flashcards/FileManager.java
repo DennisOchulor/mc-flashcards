6b52a3f3bf2916478cdacd47bee955041c80e92f
Dennis Ochulor <dennisnwanevu@gmail.com>
2024-06-19 16:31:10 +0800

    Bump to v1.1.0, added Anki import

--- src/main/java/io/github/dennisochulor/flashcards/FileManager.java
+++ src/main/java/io/github/dennisochulor/flashcards/FileManager.java
@@ -12,6 +12,9 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
 
 @Environment(EnvType.CLIENT)
 public class FileManager {
@@ -33,6 +36,7 @@ public class FileManager {
                 Files.copy(FileManager.class.getResourceAsStream("/flashcards/config.json"),Path.of(dotMinecraftFolder + "/config/flashcards/config.json"));
                 Files.copy(FileManager.class.getResourceAsStream("/flashcards/questions/default.json"),Path.of(questionsFolder + "/default.json"));
             }
+            importQuestions();
         }
         catch (IOException e) {
             throw new RuntimeException(e);
@@ -43,7 +47,18 @@ public class FileManager {
 
     public static ModConfig getConfig() {
         try {
-            return new Gson().fromJson(Files.readString(configFile.toPath()),ModConfig.class);
+            // ensure questions and config are always in sync, otherwise kaboom
+            ModConfig config = new Gson().fromJson(Files.readString(configFile.toPath()),ModConfig.class);
+            AtomicBoolean changed = new AtomicBoolean(false);
+            getQuestions().forEach((category,questions) -> {
+                if(!config.categoryToggle().containsKey(category)) {
+                    config.categoryToggle().put(category,true);
+                    changed.set(true);
+                }
+            });
+            if(changed.get()) updateConfig(config);
+
+            return config;
         }
         catch (IOException e) {
             throw new UncheckedIOException(e);
@@ -66,7 +81,7 @@ public class FileManager {
     public static HashMap<String,List<Question>> getQuestions() {
         Gson gson = new GsonBuilder().create();
         HashMap<String,List<Question>> map = new HashMap<>();
-        Arrays.stream(questionsFolder.listFiles()).forEachOrdered(f -> {
+        Arrays.stream(questionsFolder.listFiles()).filter(f -> f.getName().endsWith(".json")).forEachOrdered(f -> {
             Question[] questions;
             try {
                 questions = gson.fromJson(Files.readString(f.toPath()), Question[].class);
@@ -81,7 +96,7 @@ public class FileManager {
     public static void updateQuestions(HashMap<String,List<Question>> map) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try {
-            Arrays.stream(questionsFolder.listFiles()).forEach(File::delete);
+            Arrays.stream(questionsFolder.listFiles()).filter(f -> f.getName().endsWith(".json")).forEach(File::delete);
 
             for (Map.Entry<String, List<Question>> entry : map.entrySet()) {
                 String filename = entry.getKey();
@@ -95,6 +110,38 @@ public class FileManager {
         catch (IOException e) {
             throw new UncheckedIOException(e);
         }
+    }
+
+    private static void importQuestions() {
+        File[] files = questionsFolder.listFiles();
+        Stream<File> stream = Arrays.stream(files).filter(f -> f.getName().endsWith(".txt"));
+        importAnki(stream.filter(f -> f.getName().contains("anki")));
+    }
+
+    private static void importAnki(Stream<File> stream) {
+        stream.forEach(f -> {
+            try {
+                List<Question> importedQuestions = new ArrayList<>();
+                List<String> lines = Files.readAllLines(f.toPath());
+
+                lines.stream().filter(line -> !line.startsWith("#")).forEach(line -> {
+                    String[] q =  line.split("\t",2);
+                    importedQuestions.add(new Question(q[0],q[1]));
+                });
+
+                String filename = "anki-" + ThreadLocalRandom.current().nextInt(0,1000);
+                FileWriter writer = new FileWriter(questionsFolder.toPath() + "/" + filename + ".json");
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                gson.toJson(importedQuestions.toArray(), Question[].class, writer);
+                writer.flush();
+                writer.close();
+
+                f.delete();
+            }
+            catch (Exception e) {
+                ClientModInit.LOGGER.warn(f.getName() + " encountered an error during import!", e);
+            }
+        });
     }
 
 }
